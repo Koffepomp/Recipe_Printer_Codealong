a23c28a13a9635210f0b890c209df14b7d3c1746
Koffepomp <Zephs1986@gmail.com>
2022-09-21 08:28:28 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
 ﻿// Fråga användaren om en ingrediens
-// Lägg denna ingrediens i en lista med strängar
-// 
+// IF det finns en ingrediens - lägg denna ingrediens i en lista med strängar
+// ELSE IF användaren skrev Done - printa alla ingredienser
+// ELSE IF användaren skrev remove - fråga vilken ingrediens den vill ta bort
+// ELSE fråga användaren om en ingrediens
+
